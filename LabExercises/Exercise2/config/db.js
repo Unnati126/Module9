4beb99bdea639dc5b33b7c778eeabc9230b005de
Unnati Gadhiya <unnatigadhiya0206@gmail.com>
2025-06-25 15:07:46 +1000

    init commit

--- LabExercises/Exercise2/config/db.js
+++ LabExercises/Exercise2/config/db.js
@@ -3,7 +3,8 @@ require("dotenv").config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await //mongoose.connect(process.env.MONGO_URI);
+        mongoose.connect('mongodb://127.0.0.1:27017/blogdb');
         console.log("MongoDB connected successfully");
     } catch (error) {
         console.error("MongoDB connection failed:", err);
