fedf720550ef5c86e3e44d81138329f31d2214ad
Unnati Gadhiya <unnatigadhiya0206@gmail.com>
2025-06-27 18:48:53 +1000

    init commit

--- mysqldb-app/controllers/userController.js
+++ mysqldb-app/controllers/userController.js
@@ -21,6 +21,29 @@ res.send({ result: 500, error: err.message });
 })
 }
 
+// uses JSON from request body to update user ID from params
+const updateUser = (req, res) => {
+Models.User.update(req.body, { where: { id: req.params.id },
+returning: true })
+.then(data => {
+res.send({ result: 200, data: data });
+}).catch((err) => {
+console.log(err);
+res.send({ result: 500, error: err.message });
+});
+};
+
+// deletes user matching ID from params
+const deleteUser = (req, res) => {
+Models.User.destroy({ where: { id: req.params.id } })
+.then(data => {
+res.send({ result: 200, data: data });
+}).catch((err) => {
+console.log(err);
+res.send({ result: 500, error: err.message });
+});
+};
+
 module.exports = {
-getUsers, createUser
-}
+getUsers, createUser, updateUser, deleteUser
+};
--- mysqldb-app/routes/userRoutes.js
+++ mysqldb-app/routes/userRoutes.js
@@ -13,4 +13,14 @@ router.post('/create', (req, res) => {
 Controllers.userController.createUser(req.body, res)
 })
 
+// matches PUT requests to /api/users/123 (stores 123 in id param)
+router.put('/:id', (req, res) => {
+Controllers.userController.updateUser(req, res)
+})
+
+// matches DELETE requests to /api/users/123 (123 in id param)
+router.delete('/:id', (req, res) => {
+Controllers.userController.deleteUser(req, res)
+})
+
 module.exports = router;
