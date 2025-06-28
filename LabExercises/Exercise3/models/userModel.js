38f6661bcdac0e105ec6e93ffdfd1ef0d50fc393
Unnati Gadhiya <unnatigadhiya0206@gmail.com>
2025-06-28 18:52:32 +1000

    init commit

--- LabExercises/Exercise3/models/userModel.js
+++
@@ -1,7 +0,0 @@
-const db = require("../config/db");
-
-// Create a new user
-exports.createUser = (username, email, callback) => {
-  const sql = "INSERT INTO users (username, email) VALUES (?, ?)";
-  db.query(sql, [username, email], callback);
-};
